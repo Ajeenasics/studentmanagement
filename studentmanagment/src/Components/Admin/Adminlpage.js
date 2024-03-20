3c3526a8fc2d98dacfee21a160864ad9bfc2612c
vinukamalk <vinukamal59@gmail.com>
2024-03-20 15:21:57 +0530

    hy

---
+++ studentmanagment/src/Components/Admin/Adminlpage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Adminlpage() {
+  return (
+    <div>
+          <div>
+            <div>
+                <img src={pic} alt='image'></img>
+            </div>
+            <div className='text'>
+                <h2>Hi </h2>
+                <h1>ADMIN!!</h1>
+
+            </div>
+            <div className='login'>
+                <button type="button" class="btn btn-outline-info">LOGIN</button>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Adminlpage
